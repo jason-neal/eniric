24d2d85069051c255ec9224408f5793670ddb6a9
Jason Neal <jason.neal@astro.up.pt>
2016-12-14 17:19:30 +0000

    Add code to modify, with an original copy for comparisons
    Former-commit-id: c884065f8c031c887b44926e0177ebe27a082738

---
+++ eniric/Qcalculator.py
@@ -0,0 +1,32 @@
+"""
+Created on Mon Dec 29 00:14:56 2014
+
+@author: pfigueira
+"""
+
+from IOmodule import read_2col
+
+import numpy as np
+
+c =  	299792458 #m/s
+
+def RVprec_calc(spectrum_file= "resampled/Spectrum_M0-PHOENIX-ACES_Hband_vsini1.0_R60k_res3.txt"):
+    """
+    function that claculates the RV precision achievable on a spectrum 
+    """
+    wavelength, flux = read_2col(spectrum_file)
+    
+    return [c / SqrtSumWis(wavelength, flux)]
+    
+def SqrtSumWis(wavelength, flux):
+    """
+    Calculation of the SquareRoot of the sum of the Wis for a spectrum
+    """
+    
+    delta_F = (np.array(flux[1:]) - np.array(flux[:-1]))
+    delta_l = np.array(wavelength[:-1])
+
+    derivF_over_lambda = delta_F/delta_l
+    
+    return np.sqrt( np.sum( np.array(wavelength[:-1])**2.0 * derivF_over_lambda **2.0 / np.array(flux[:-1]) ) )
+    
