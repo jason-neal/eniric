53d25d0246fb6467b661566dce35b789bb78a54f
Jason Neal <jason.neal@astro.up.pt>
2016-12-20 13:48:23 +0000

    parallel option in tests, comment out old version.
    Former-commit-id: ad343a15c5662984d249efde778c8ceb88bf9fa0 [formerly 68d2297516bef916cd1f5b11ca6dddb1b725d9ec]
    Former-commit-id: c5b648e0c77e216b5864cf90ba8d54fc7b03e494

--- bin/nIR_testing.py
+++ bin/nIR_testing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # Testing script for nIR analysis
+# Run new and old code to test output.S
 # Jason Neal
 # December 2016
 from __future__ import division, print_function
@@ -23,12 +24,13 @@ vsini = 1
 epsilon = 0.6
 FWHM_lim = 5
 plot = False
+numProcs = 4
 # print("readin =", read_spectrum(spectrum))  # takes a bit of time
 
 # New version
 start_time = datetime.datetime.now()
 print("Time at start of new code", start_time)
-wav_band, flux_conv_res = convolution(spectrum_path, band, vsini, R, epsilon, FWHM_lim, plot)  # takes a very long time. good progress indicator though
+wav_band, flux_conv_res = convolution(spectrum_path, band, vsini, R, epsilon, FWHM_lim, plot, numProcs=numProcs)
 end_time = datetime.datetime.now()
 print("Time at end of new code", end_time)
 print("Time to run new convolution = {}".format((end_time-start_time)))
@@ -36,20 +38,17 @@ print("Time to run new convolution = {}".format((end_time-start_time)))
 resample_allfiles()
 
 # The unchanged version
-old_start_time = datetime.datetime.now()
-print("Time at start of old code", old_start_time)
-old_wav_band, old_flux_conv_res = old_convolution(spectrum_path, band, vsini, R, epsilon, FWHM_lim, plot)  # takes a very long time. good progress indicator though
-old_end_time = datetime.datetime.now()
-print("Time at end of old code", old_end_time)
-print("Time to run old convolution = {}".format((end_time-start_time)))
+# old_start_time = datetime.datetime.now()
+# print("Time at start of old code", old_start_time)
+# old_wav_band, old_flux_conv_res = old_convolution(spectrum_path, band, vsini, R, epsilon, F FWHM_lim, plot)  # takes a very long time. good progress indicator though
+# old_end_time = datetime.datetime.now()
+# print("Time at end of old code", old_end_time)
+# print("Time to run old convolution = {}".format((end_time-start_time)))
 
-old_resample_allfiles()
+# old_resample_allfiles()
 
 # Plot results together
-plt.plot(old_wav_band, old_flux_conv_res, label='Old code')
-plt.plot(wav_band, flux_conv_res, label='New code')
-plt.legend(loc=0)
-plt.show()
-
-# Test rotation kernal
-# test_rotation_kernal(vsini, epsilon)
+# plt.plot(old_wav_band, old_flux_conv_res, label='Old code')
+# plt.plot(wav_band, flux_conv_res, label='New code')
+# plt.legend(loc=0)
+# plt.show()
