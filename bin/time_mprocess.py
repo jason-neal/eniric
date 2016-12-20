aa7f5b0fb22d197f4bc2b45e55b88b104a469485
Jason Neal <jason.neal@astro.up.pt>
2016-12-20 13:41:35 +0000

    Time convolution tests. Parallel is only slightly faster on high process numbers in K band. Process of 1 is slower then 0. J band multiprocess is about a second slower.
    Former-commit-id: 2ab2d2947fe3d8acbfbfeee441eacd8b16716edc [formerly 80d213b4de7eee763dff6355f22c79d25c3c0739]
    Former-commit-id: a2e23951ca37fd861f42e38ef1b033cb5a2c51a9

---
+++ bin/time_mprocess.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Script to time convolution using different number of processors.
+# Jason Neal
+# December 2016
+from __future__ import division, print_function
+from eniric.nIRanalysis import convolution
+import datetime
+
+spectrum_name = "lte03900-4.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_wave.dat"
+
+data_rep = "../data/nIRmodels/"
+results_dir = "../data/results/"
+
+spectrum_path = data_rep + "PHOENIX-ACES/PHOENIX-ACES-AGSS-COND-2011-HiRes/" + spectrum_name
+# Some test parameters
+band = "K"
+R = 100000
+vsini = 1
+epsilon = 0.6
+FWHM_lim = 5
+plot = False
+numProcs = 0
+
+numprocs = [None, 0, 1, 2, 3, 4]
+
+def time_diff_procs(numProcs):
+    """ Time the convolution with different number of processors"""
+    conv_times = dict()
+    for proc in numProcs:
+        start_time = datetime.datetime.now()
+        __, __ = convolution(spectrum_path, band, vsini, R, epsilon, FWHM_lim, plot, numProcs=proc)
+        end_time = datetime.datetime.now()
+        conv_times[proc] = end_time - start_time
+    return conv_times
+
+conv_times = time_diff_procs(numprocs)
+
+print("Num Processors\t Time")
+
+for key in bunprocs:
+    print("{0}\t{1}".format(key, conv_times[key]))
