0e86219f87cbb50bcb2b2a8c6d1dd714d955a6f0
Jason Neal <jason.neal@astro.up.pt>
2016-12-21 16:12:00 +0000

    Script to perge result and resampled files
    Former-commit-id: 63b3137ae2f0035b6421de237fc7fca46fed3f59 [formerly 5c9152b6a1bf3d40dde717369d9aaba61b38a1b1]
    Former-commit-id: ac77ef5b66e1cffd6ced791b016befa81ab7c38c

---
+++ bin/perge_results.sh
@@ -0,0 +1,9 @@
+## Run this at your own peril!
+# Deletes all the results to start over.
+# remember the original code takes and hour and a hald each
+rm ../data/results/*
+rm ../data/resampled/results/*
+rm ../data/resampled/*
+rm ../data/original_code/results/*
+rm ../data/original_code/resampled/original_code/results/*
+rm ../data/original_code/resampled/*
