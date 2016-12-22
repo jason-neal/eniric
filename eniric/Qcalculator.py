0901eda1b3b5fe04500772fc4b98fb40bf7d8d5c
Jason Neal <jason.neal@astro.up.pt>
2016-12-22 13:51:03 +0000

    RVcalc for transmission and chunks: Updated Qcalculate with the improved code found on exo.
    Former-commit-id: 92708fff7bf6af75021da04ac97aad40edc4c657 [formerly c4299eec2ca96f63938fcb412b556cdb3649f0b4]
    Former-commit-id: 9224d2a6a1f4aa8f558c9aee68fc8fbe9206e0b6

--- eniric/Qcalculator.py
+++ eniric/Qcalculator.py
@@ -12,15 +12,17 @@ import numpy as np
 import pandas as pd
 c = 299792458  # m/s
 
-
-def RVprec_calc(spectrum_file="resampled/Spectrum_M0-PHOENIX-ACES_Hband_vsini1.0_R60k_res3.txt"):
-    """
-    function that claculates the RV precision achievable on a spectrum
-    """
+def RVprec_test(spectrum_file= "resampled/Spectrum_M0-PHOENIX-ACES_Hband_vsini1.0_R60k_res3.txt"):
     data = pd.read_table(spectrum_file, comment='#', header=None, names=["wavelength", "flux"], dtype=np.float64, delim_whitespace=True)
     wavelength, flux = data["wavelength"].values, data["flux"].values
 
-    return [c / SqrtSumWis(wavelength, flux)]
+    return RVprec_calc(wavelength, flux)
+
+def RVprec_calc(wavelength, flux):
+    """ Calculate the RV precision achievable on a spectrum.
+    """
+
+    return c / SqrtSumWis(wavelength, flux)
 
 
 def SqrtSumWis(wavelength, flux):
@@ -34,3 +36,50 @@ def SqrtSumWis(wavelength, flux):
     derivF_over_lambda = delta_F / delta_l
 
     return np.sqrt(np.sum(wavelength[:-1]**2.0 * derivF_over_lambda**2.0 / flux[:-1]))
+
+
+def RVprec_calc_chunks(wavelength, flux):
+    """
+    the same as RVprec_calc, but now wavelength and flux are organized into chunks and the weighted average formula is used
+    """
+
+    RV_vector = np.array([RVprec_calc(wav_chunk, flux_chunk) for wav_chunk, flux_chunk in zip(wavelength, flux)])
+
+    RV_value = 1.0/(np.sqrt(np.sum( (1.0/RV_vector)**2.0 )))
+
+    return RV_value
+
+###############################################################################
+
+def RV_prec_calc_Trans(wavelength, flux, transmission):
+    """
+    The same as RV_prec_calc, but considering a transmission different than zero
+    """
+    return c / SqrtSumWisTrans(wavelength, flux, transmission)
+
+
+def SqrtSumWisTrans(wavelength, flux, transmission):
+    """
+    Calculation of the SquareRoot of the sum of the Wis for a spectrum, considering transmission
+
+    Parameters
+    ----------
+    wavelength: array-like
+        Wavelength array
+    flux: array-like
+        Flux array
+    transmission: array-like
+        Transmission array
+
+    Returns
+    -------
+    SqrtSumWisTrans: array-like
+        Squarerooted sum of pixel weigths including transmission.
+    """
+
+    delta_F = np.diff(flux)
+    delta_l = np.diff(wavelength)
+
+    derivF_over_lambda = delta_F/delta_l
+
+    return np.sqrt(np.sum(wavelength[:-1]**2.0 * derivF_over_lambda**2.0 / (flux[:-1])/(transmission[:-1]**2.0))))
