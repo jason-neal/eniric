Fix delta_l in Qcalculator. Was missing the gradient had just one of the values. Fixed it in both code versions
Former-commit-id: 55db09b0a375a0e37adaa3cf578c5503a334d9e6 [formerly c11c35c771b7d490454c18fdd4c7765989d9d726 [formerly 48dcd812c63d4783ea0bbc547bdbf98b7493cd47]]
Former-commit-id: c11c35c771b7d490454c18fdd4c7765989d9d726
Former-commit-id: f1ee42c7f2e316c8ef7f244c8452660d92dd5431

eniric/Qcalculator.py
@@ -29,8 +29,8 @@ def SqrtSumWis(wavelength, flux):
     """
 
     delta_F = np.diff(flux)
-    delta_l = wavelength[:-1]
+    delta_l = np.diff(wavelength)
 
-    derivF_over_lambda = delta_F/delta_l
+    derivF_over_lambda = delta_F / delta_l
 
     return np.sqrt(np.sum(wavelength[:-1]**2.0 * derivF_over_lambda**2.0 / flux[:-1]))