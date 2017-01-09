21fdc05b873540e72c914d5c5426e3935ebb94e7
Jason Neal <jason.neal@astro.up.pt>
2017-01-09 18:58:56 +0000

    Start of script to run convolutions on all data we want
    Former-commit-id: 65ee19a818ed2e72ecbf4bc191ca81b86384c023 [formerly 208c011eadb2437dd1aee24bef0898b0152af963]
    Former-commit-id: 7139cd8f2998ccbf30c21634abe3bdfd80d8b580

---
+++ bin/process_all.py
@@ -0,0 +1,29 @@
+# process_all.py
+
+"""
+Script to run all convolutions for the new NIR analysis.
+Includes adding metalicity and logg parameters
+
+Jason Neal, Janurary 2017
+
+"""
+
+import numpy as np
+import itertools
+
+fehs = np.arange(-2, 1, 0.5)
+spec_type = ["M0", "M3", "M6", "M9"]
+loggs = np.arange(3.0, 6.5, 0.5)
+alphas = [0.0]
+spectrum_iter = itertools.product(spec_type, loggs, fehs, alphas)
+
+resolution = [60000, 80000, 100000]
+vsini = [1.0, 5.0, 10.0]
+sampling = [1, 3, 6]   # effect of sampling on precision
+
+convolution_iter = itertools.product(resolution, vsini)
+
+
+if __name__ == __main__:
+    # do convolutions
+    pass
