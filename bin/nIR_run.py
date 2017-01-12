c0f8cb71919c3fb2a487ec73fcfde6353b8473a9
Jason Neal <jason.neal@astro.up.pt>
2017-01-12 10:22:49 +0000

    Add multiple args for startype, vsini, resolution, band, and sample_rate.
    Loop over startype, and remove list calls as this is handled in argparse now.
    
    
    Former-commit-id: 46002cc344a42673f64fc7ec194dfb18b1fd81c7 [formerly aa634e42479e6a4388c9f35b929cc6ba7162f105]
    Former-commit-id: 406732084d616059a879e06879de58e62e03b02a

--- bin/nIR_run.py
+++ bin/nIR_run.py
@@ -14,16 +14,16 @@ def _parser():
     :returns: the args
     """
     parser = argparse.ArgumentParser(description='Helpful discription')
-    parser.add_argument('startype', help='Spectral Type e.g "MO"', type=str)
+    parser.add_argument('startype', help='Spectral Type e.g "MO"', type=str, nargs="+")
     parser.add_argument("-v", "--vsini", help="Rotational velocity of source",
-                        type=float)
+                        type=float, nargs="+")
     parser.add_argument("-R", "--resolution", help="Observational resolution",
-                        type=float)
+                        type=float, nargs="+")
     parser.add_argument("-b", "--band", type=str, default="ALL",
                         choices=["ALL", "VIS", "GAP", "Z", "Y", "J", "H", "K"],
-                        help="Wavelength band to select")
+                        help="Wavelength band to select", nargs="+")
     parser.add_argument('-d', '--data_dir', help='Data directory', type=str, default=None)
-    parser.add_argument('--sample_rate', default=3.0, type=float,
+    parser.add_argument('--sample_rate', default=[3.0], type=float, nargs="+",
                         help="Resample rate, pixels per FWHM. Default=3.0")
     parser.add_argument('--results', default=None, type=str,
                         help='Result directory Default=data_dir+"/../"')
@@ -54,10 +54,30 @@ def get_spectrum_name(startype, logg="4.50", feh="0.0"):
 
 def main(startype, vsini, resolution, band, data_dir=None, results=None,
          resamples=None, sample_rate=3.0, noresample=False, normalize=False):
+    """ Run convolutions of NIR spectra for the range of given parameters.
 
+    Multiple values of startype, vsini, resolution, band, and sample_rate can
+    be provided.
+
+    Read files from data_dir + "PHOENIX_ACES_spectra/"
+    Saves results to data_dir + "results/"
+    Resamples results to data_dir + "resamples/"
+
+    Parameters
+    ----------
+    startype: list of strings
+    vsini: list of floats
+    resolution: list of floats
+    band: list of strings
+    data_dir: str, default=None
+    results: str, default=None
+    resample: str, default=None
+    sample_rate: list of floats default=[3.0]
+    noresample: bool default=False
+    normalize: bool default=False
+    
+    """
         # vsini, resolution, band and sample_rate can all be a series of values
-
-    spectrum_name = get_spectrum_name(startype)
 
     if data_dir is None:
         data_dir = "../data/"
@@ -72,35 +92,29 @@ def main(startype, vsini, resolution, band, data_dir=None, results=None,
     else:
         resampled_dir = resamples
 
-    if not isinstance(vsini, list):
-        vsini = [vsini]
-    if not isinstance(resolution, list):
-        resolution = [resolution]
-    if not isinstance(band, list):
-        band = [band]
-    if not isinstance(sample_rate, list):
-            sample_rate = [sample_rate]
+    for star in startype:
+        spectrum_name = get_spectrum_name(star)
 
-    for vel in vsini:
-        for R in resolution:
-            for b in band:
-                for sample in sample_rate:
-                    if normalize:
-                        # when normalize ation is confirmed then can
-                        result_name = "Spectrum_{}-PHOENIX-ACES_{}band_vsini{}_R{}k_conv_normalized.txt".format(startype, b, vel, int(R/1000))
+        for b in band:
+            for vel in vsini:
+                for R in resolution:
+                    for sample in sample_rate:
+                        if normalize:
+                            # when normalize ation is confirmed then can
+                            result_name = "Spectrum_{}-PHOENIX-ACES_{}band_vsini{}_R{}k_conv_normalized.txt".format(star, b, vel, int(R/1000))
+                        else:
+                            result_name = "Spectrum_{}-PHOENIX-ACES_{}band_vsini{}_R{}k.txt".format(star, b, vel, int(R/1000))
+                        print("Name to be result file", result_name)
+                        convolve_spectra(data_dir + spectrum_name, b, vel, R, epsilon=0.6, plot=False,
+                                         FWHM_lim=5.0, numProcs=None, data_rep=data_dir,
+                                         results_dir=results_dir, normalize=normalize, output_name=result_name)
+
+                    # Resample only the file just made
+                    if noresample:
+                        pass
                     else:
-                        result_name = "Spectrum_{}-PHOENIX-ACES_{}band_vsini{}_R{}k.txt".format(startype, b, vel, int(R/1000))
-                    print("Name to be result file", result_name)
-                    convolve_spectra(data_dir + spectrum_name, b, vel, R, epsilon=0.6, plot=False,
-                                     FWHM_lim=5.0, numProcs=None, data_rep=data_dir,
-                                     results_dir=results_dir, normalize=normalize, output_name=result_name)
-
-                # Resample only the file just made
-                if noresample:
-                    pass
-                else:
-                    resampler(result_name, results_dir=results_dir,
-                               resampled_dir=resampled_dir, sampling=sample)
+                        resampler(result_name, results_dir=results_dir,
+                                   resampled_dir=resampled_dir, sampling=sample)
 
 
 if __name__ == '__main__':
