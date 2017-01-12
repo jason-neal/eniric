e818ae213eb492b28c607eca7cf57b9a735fea8e
Jason Neal <jason.neal@astro.up.pt>
2017-01-12 23:39:21 +0000

    Make default normalize=True
    Make unnormalized files have "_unnormalized" appended.
    
    
    Former-commit-id: 40e21649c3cb4054b90f7f002daec5cbda430c5d [formerly 52f2394ed231c97e844637ed635f30bfc99d5382 [formerly 387ec8c8f010792186f3c8c9f928dae99a725f57]] [formerly e40203c83f0c84ac6ab1fcdf1715266a882c517f [formerly 52f2394ed231c97e844637ed635f30bfc99d5382 [formerly 387ec8c8f010792186f3c8c9f928dae99a725f57] [formerly 606e319a316f9577d6918d155a30f65bb73e9353 [formerly 83291c37a97ecceb9dc1aed0525aee8032630d0c]]]]
    Former-commit-id: 9698a24ce2cac1e3bf980de2b06beb7ca777e6b7 [formerly 1ca7baffb9678e3bbb3957cc4bcd8e2cb6bb7ec0]
    Former-commit-id: 062653492bc7495b86ac50001d3b3a8e3272cb18
    Former-commit-id: 205c2d78a225fb99ca02712629eab6f3c3c14948 [formerly 448b41cf46c314d32b6702312e93a1f05b1955ea]
    Former-commit-id: d6ec041dc24cea4381423a75efcacce11f1f8d1d

--- eniric/nIRanalysis.py
+++ eniric/nIRanalysis.py
@@ -67,7 +67,7 @@ def save_convolution_results(filename, wavelength, flux, convolved_flux):
 
 def convolve_spectra(spectrum, band, vsini, R, epsilon=0.6, FWHM_lim=5.0,
                      plot=True, numProcs=None, results_dir=results_dir,
-                     data_rep=data_rep, normalize=False, output_name=None):
+                     data_rep=data_rep, normalize=True, output_name=None):
     """ Load Spectrum, apply convolution and then save results.
 
     """
@@ -81,8 +81,12 @@ def convolve_spectra(spectrum, band, vsini, R, epsilon=0.6, FWHM_lim=5.0,
 
     if output_name is None:
         name_model = name_assignment(spectrum)
-        filename = ("{0}Spectrum_{1}_{2}band_vsini{3:3.1f}_R{4:d}k.txt"
-                    "").format(results_dir, name_model, band, vsini, R/1000)
+        if normalize:
+            filename = ("{0}Spectrum_{1}_{2}band_vsini{3:3.1f}_R{4:d}k.txt"
+                        "").format(results_dir, name_model, band, vsini, R/1000)
+        else:
+            filename = ("{0}Spectrum_{1}_{2}band_vsini{3:3.1f}_R{4:d}k_unnormalized.txt"
+                        "").format(results_dir, name_model, band, vsini, R/1000)
     else:
         filename = results_dir + output_name
 
@@ -104,7 +108,7 @@ def convolve_spectra(spectrum, band, vsini, R, epsilon=0.6, FWHM_lim=5.0,
 
 
 def convolution(wav, flux, vsini, R, band="All", epsilon=0.6, FWHM_lim=5.0,
-                numProcs=None, normalize=False, output_name=None):
+                numProcs=None, normalize=True, output_name=None):
     """ Perform convolution of spectrum.
 
     Rotational convolution followed by a guassian a a specified resolution R.
@@ -134,6 +138,7 @@ def convolution(wav, flux, vsini, R, band="All", epsilon=0.6, FWHM_lim=5.0,
     flux_conv: ndarray
         Convolved flux for the selected band.
     """
+
     wav_band, flux_band = band_selector(wav, flux, band)
 
     # We need to calculate the FWHM at this value in order to set the starting point for the convolution
@@ -169,7 +174,7 @@ def convolution(wav, flux, vsini, R, band="All", epsilon=0.6, FWHM_lim=5.0,
 
 
 def rotational_convolution(wav_extended, wav_ext_rotation, flux_ext_rotation,
-                           vsini, epsilon, numProcs=None, normalize=False):
+                           vsini, epsilon, numProcs=None, normalize=True):
     """ Perform Rotational convolution part of convolution.
     """
 
@@ -229,7 +234,7 @@ def rotational_convolution(wav_extended, wav_ext_rotation, flux_ext_rotation,
 
 
 def resolution_convolution(wav_band, wav_extended, flux_conv_rot, R, FWHM_lim,
-                           numProcs=1, normalize=False):
+                           numProcs=1, normalize=True):
     """ Perform Resolution convolution part of convolution.
     """
 
