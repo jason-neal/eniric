4e78e1d39bb6548066e1424edf6a467d0cf1a5d1
Jason Neal <jason.neal@astro.up.pt>
2017-01-18 00:31:13 +0000

    Normalization constant functions for a band or a wave value. The band version calls the wave version after using band_limits()  to determine the middle of the band.

--- eniric/snr_normalization.py
+++ eniric/snr_normalization.py
@@ -4,10 +4,15 @@
 For consistancy and valid comparision we normalize each spectra to acheive a consistant SNR at a specific location. """
 
 # Normaize to SNR 100 in middle of J band 1.25 micron!
+import numpy as np
+from eniric.utilities import band_limits
 
-""" This is the original values to acheive a SNR of 100 in the middle of the J band at 1.25 micron """
+
 def normalize_flux(flux_stellar, id_string):
     """Normalize flux to have SNR of 100 in middle of J band."""
+
+    """ This is the original values to acheive a SNR of 100 in the middle of the J band at 1.25 micron """
+
 
     if("M0" in id_string):
         norm_constant = 1607
@@ -37,30 +42,83 @@ def normalize_flux(flux_stellar, id_string):
     return flux_stellar / ((norm_constant / 100.0)**2.0)
 
 
-def determine_constant(wav, flux, SNR=100, band="J"):
-    """ Determine the constant to acheive a SNR in the middle of band."
 
+
+def snr_constant_band(wav, flux, SNR=100, band="J"):
+    """ Determine the constant to acheive a SNR in the middle of a given band."
+
+    SNR estimated by the square root of the number of photons in a resolution element.
+
+    Parameters
+    ----------
     wav: ndarray (micron)
     flux: ndarray (photons/s/cm**2)
     SNR: int,  default = 100
     band: str, default = "J"
+
+    Returns
+    -------
+    normalization_value: float
+
     """
-    band_mid = {"J": 1.25}
+
+    band_min, band_max = band_limits(band)
+
+    band_middle = (band_min + band_max) / 2
+
+    if band_middle < wav[0] or band_middle > wav[-1]:
+        # not in range
+        pass
     # Option to specify own wavelength?
-    index_reference = np.searchsorted(wav, band_mid[band])  # Searching for the closest index
-
-    SN_estimate = np.sqrt(np.sum(flux_stellar[index_reference-1:index_reference+2]))
-
-    print("\tSanity Check: The S/N for the {:s} reference model was of {:4.2f}.".format(id_string, SN_estimate))
-
-    norm_constant = (SN_estimate / SNR)**2
+    norm_constant = snr_constant_wav(wav, flux, band_middle, SNR=SNR)
 
     # Test it
-    flux2 = flux / norm_constant
-    SN_estimate2 = np.sqrt(np.sum(flux2[index_reference-1:index_reference+2]))
-    print("\tSanity Check: The S/N for the {:s} normalized result was of {:4.2f}.".format(id_string, SN_estimate))
+    # flux2 = flux / norm_constant
 
     return norm_constant
+
+
+def snr_constant_wav(wav, flux, wav_ref, SNR=100, sampling=3):
+    """ Determine the constant to acheive a SNR at given wavelength."
+
+    SNR estimated by the square root of the number of photons in a resolution element.
+
+    Parameters
+    ----------
+    wav: ndarray
+        Wavlength in micron
+    flux: ndarray
+        Photon flux array (photons/s/cm**2)
+    wav_ref: float
+        Wavelength to set the SNR per resolution element.
+    SNR: int,  default = 100
+        SNR to set.
+    sampling: int
+       Number of pixels per resolution element.
+
+    Returns
+    -------
+    norm_value: float
+        Normalization value to divide the flux by to acheive the desired SNR "SNR"
+        in resolution element (defined by "sampling") around the wavelength "wav_ref".
+
+    Notes
+    -----
+    We want to be consistent for each spectra. If we specify the middle of J band
+    as the reference it will need to be used for all bands of that spectra.
+
+    """
+
+    index_ref = np.searchsorted(wav, wav_ref)  # Searching for the closest index
+
+    indexes = sampling_index(index_ref, sampling=sampling, array_length=len(wav))
+
+    SN_estimate = np.sqrt(np.sum(flux[indexes]))
+
+    # print("\tSanity Check: The S/N for the reference model was of {:4.2f}.".format(SN_estimate))
+    norm_value = (SN_estimate / SNR)**2
+    return norm_value
+
 def sampling_index(index, sampling=3, array_length=None):
     """ Get a small number of index values around the given index value.
 
