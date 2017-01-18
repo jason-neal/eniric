eaca6462ae3e8f123bb9a3b614073c3a8dc46057
Jason Neal <jason.neal@astro.up.pt>
2017-01-18 00:03:58 +0000

    Separate band limits into separate function. which is called from band_selector. This gives option to just get the limit values for a band.
    Added tests too.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -3,6 +3,7 @@
 import pytest
 import numpy as np
 from eniric.utilities import get_spectrum_name, wav_selector
+import eniric.utilities as utils
 
 # Test using hypothesis
 from hypothesis import given, example
@@ -53,3 +54,49 @@ def test_wav_selector(x, y, wav_min, wav_max):
     assert len(x1) == len(y1)
     assert isinstance(x1, np.ndarray)
     assert isinstance(y1, np.ndarray)
+
+
+def test_band_limits():
+    """ Test geting limits out of band. """
+
+    # Test all bands, lower case allowed
+    for band in ["VIS", "GAP", "z", "Y", "h", "J", "K", "CONT", "NIR"]:
+        band_min, band_max = utils.band_limits(band)
+        assert band_min < band_max
+        assert band_min != band_max
+
+    # Test non-bands
+    with pytest.raises(ValueError):
+        utils.band_limits("X")
+
+    with pytest.raises(ValueError):
+        utils.band_limits("M0")
+    with pytest.raises(AttributeError):
+        utils.band_limits(np.array(1))
+    with pytest.raises(AttributeError):
+        utils.band_limits(["list", "of", "strings"])
+
+
+def test_band_selector():
+    """ Test band selector selects the wav and flux in the given band."""
+    wav = np.linspace(0.5, 3, 100)
+    flux = wav**2
+
+    for band in ["Z", "H", "J", "k"]:
+        band_min, band_max = utils.band_limits(band)
+        assert np.any(wav < band_min)      # Assert wav goes outside band
+        assert np.any(wav > band_max)
+
+        wav2, flux2 = utils.band_selector(wav, flux, band)
+        assert np.all(wav2 > band_min)
+        assert np.all(wav2 < band_max)
+
+    # Test it also raises the Value and Attribute Errors
+    with pytest.raises(ValueError):
+        utils.band_selector(wav, flux, "M0")
+    with pytest.raises(AttributeError):
+        utils.band_selector(wav, flux, 1)
+    with pytest.raises(AttributeError):
+        utils.band_selector(wav, flux, ["list", "of", "strings"])
+    with pytest.raises(AttributeError):
+        utils.band_selector(wav, flux, flux)
