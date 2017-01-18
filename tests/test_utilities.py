4f5c52ebb01fe41c18da29f0c3f44dd4a3e20cc1
Jason Neal <jason.neal@astro.up.pt>
2017-01-18 00:37:09 +0000

    Declare utils module in testing the utilities.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,11 +2,11 @@
 
 import pytest
 import numpy as np
-from eniric.utilities import get_spectrum_name, wav_selector
+# from eniric.utilities import utils.get_spectrum_name, wav_selector, read_spectrum
 import eniric.utilities as utils
 
 # Test using hypothesis
-from hypothesis import given, example
+from hypothesis import given
 import hypothesis.strategies as st
 
 
@@ -14,38 +14,38 @@ def test_get_spectrum_name():
     """ Test specifing file names with stellar parameters."""
     test = ("PHOENIX-ACES_spectra/Z-0.0/lte02800-4.50"
             "-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_wave.dat")
-    assert get_spectrum_name("M6") == test
+    assert utils.get_spectrum_name("M6") == test
 
     test_alpha = ("PHOENIX-ACES_spectra/Z-0.0.Alpha=+0.20/"
                   "lte02600-6.00-0.0.Alpha=+0.20.PHOENIX-ACES-AGSS-COND-2011-HiRes_wave.dat")
-    assert get_spectrum_name("M9", logg=6, alpha=0.2) == test_alpha
+    assert utils.get_spectrum_name("M9", logg=6, alpha=0.2) == test_alpha
 
     test_pos_feh = ("PHOENIX-ACES_spectra/Z+0.5/"
                     "lte03500-0.00+0.5.PHOENIX-ACES-AGSS-COND-2011-HiRes_wave.dat")
-    assert get_spectrum_name("M3", logg=0, feh=0.5, alpha=0.0) == test_pos_feh
+    assert utils.get_spectrum_name("M3", logg=0, feh=0.5, alpha=0.0) == test_pos_feh
 
     # Catch Errors
     with pytest.raises(NotImplementedError):
-        get_spectrum_name("K1")       # Stellar type not added
+        utils.get_spectrum_name("K1")       # Stellar type not added
     with pytest.raises(NotImplementedError):
-        get_spectrum_name("A8")       # Stellar type not added
+        utils.get_spectrum_name("A8")       # Stellar type not added
     with pytest.raises(ValueError):
-        get_spectrum_name("MO")       # Miss spelled M0
+        utils.get_spectrum_name("MO")       # Miss spelled M0
     with pytest.raises(ValueError):
-        get_spectrum_name("X10")      # Not valid spectral type in [OBAFGKML]
+        utils.get_spectrum_name("X10")      # Not valid spectral type in [OBAFGKML]
 
 
 def test_org_name():
-    """ Test org flag of get_spectrum_name, suposed to be temporary."""
+    """ Test org flag of utils.get_spectrum_name, suposed to be temporary."""
     test_org = "PHOENIX-ACES_spectra/lte03900-4.50-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes_wave.dat"
-    assert get_spectrum_name("M0", org=True) == test_org
+    assert utils.get_spectrum_name("M0", org=True) == test_org
 
 
 @given(st.lists(st.floats()), st.floats(), st.floats(), st.floats())
 def test_wav_selector(x, y, wav_min, wav_max):
     """Test some properties of wavelength selector"""
     y = [xi + y for xi in x]   # just to make y different
-    x1, y1 = wav_selector(x, y, wav_min, wav_max)
+    x1, y1 = utils.wav_selector(x, y, wav_min, wav_max)
 
     # All values in selected should be less than the max and greater
     # than the min value.
