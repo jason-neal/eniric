0a45c0cca50c53fe15578c0984088f2dfbc30141
Jason Neal <jason.neal@astro.up.pt>
2017-01-18 22:49:19 +0000

    Fix input of check_test to be string. Add a isinstance call and mend the tests.
    Add a mypy type anotation.
    
    
    Former-commit-id: a0badcfb990bcbab44bc08b2ccb1a0e859a0eabb [formerly 92a60c0e813ce14d2b9bcd3b19a49e5a44e251fb] [formerly 2ded1c716456c0d344feed99c697d94e4e2ab35a [formerly 9b6c2aee24db8b1e8f8676e0f195f237a2feb5e9 [formerly 72631324c4cece5ef671a33a03e5c6b57ac35426] [formerly 7e7c09c953ad8260593b749059b56254034d8042 [formerly b3fc81437786688881d42ddbdb530abc00f9789c]]]]
    Former-commit-id: 99f7f1e6f5de915f25e093f4935d43a60e21cc25 [formerly 23780860e2edabdecfced0d152281156c0e19c85]
    Former-commit-id: a67adba66953a0e501adea56804e229c7a147166
    Former-commit-id: f0dd77bd7af86194ea1da72e11539a7c56187e0f [formerly b6074b04b01ac457637234b68c55ab59a54f868d]
    Former-commit-id: 785a492eed9bc8cbe2bf0f206f43c0e5ca48f38b

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -6,15 +6,15 @@ from bin.split_atmmodel import check_positive
 
 
 def test_check_positive():
-    """ Test that positive values are returned as floats
+    """ Test that positive string values are returned as floats
     and negative values as errors."""
 
     assert check_positive("1") == 1.0
-    assert check_positive(10) == 10.0
+    assert isinstance(check_positive("20"), float)
 
+    with pytest.raises(ValueError):
+        check_positive(10) == 10.0   # input must be float
     with pytest.raises(argparse.ArgumentTypeError):
         check_positive("-1")
-    with pytest.raises(argparse.ArgumentTypeError):
-        check_positive(-1)
-    with pytest.raises(argparse.ArgumentTypeError):
-        check_positive(-0.1)
+    with pytest.raises(ValueError):
+        check_positive(-1.0)
