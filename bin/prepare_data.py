fa90707af6c05babdff729e7400d5181d7d8c5a5
Jason Neal <jason.neal@astro.up.pt>
2017-01-24 16:56:28 +0000

    Remove temperature limit of 4000 and outdent block that was in else statement.

--- bin/prepare_data.py
+++ bin/prepare_data.py
@@ -89,49 +89,48 @@ def main(startype, temp, logg, metalicity, alpha, flux_type="photon", data_dir=N
                          f.endswith("PHOENIX-ACES-AGSS-COND-2011-HiRes.fits") and (re.search("03900-4.50-0.0", f) is not None))]
 
         for phoenix_file in phoenix_files:
-            if int(phoenix_file[3:8]) > 4000:
-                # Not doing temperatures over 4000 K.
-                pass
             else:
-                Z_folder = path.split("/")[-1]
-                os.makedirs(os.path.join(data_dir, Z_folder), exist_ok=True)  # make folder if doesn't exit
-                output_filename = os.path.join(data_dir, Z_folder,
-                                               phoenix_file[:-5] + file_suffix)  # Name of .dat file
 
-                spectra = fits.getdata(os.path.join(path, phoenix_file))
 
-                # Need to add conversions pedro preformed to flux!
-                """ The energy units of Phoenix fits files is erg/s/cm**2/cm
-                We transform the flux into photons in the read_spectrum()
-                function by multiplying the flux result by the wavelength (lambda)
+            Z_folder = path.split("/")[-1]
+            os.makedirs(os.path.join(data_dir, Z_folder), exist_ok=True)  # make folder if doesn't exit
+            output_filename = os.path.join(data_dir, Z_folder,
+                                           phoenix_file[:-5] + file_suffix)  # Name of .dat file
 
-                    Flux_photon = Flux_energy/Energy_photon
-                with
-                    Energy_photon = h*c/lambda
-                Flux_photon = Flux_energy * lambda / (h * c)
+            spectra = fits.getdata(os.path.join(path, phoenix_file))
 
-                Here we convert the flux into erg/s/cm**2/\mum by multiplying by 10**-4 cm/\mum
-                Flux_e(erg/s/cm**2/\mum)  = Flux_e(erg/s/cm**2/cm) * (1 cm) / (10000 \mum)
-                """
+            # Need to add conversions pedro preformed to flux!
+            """ The energy units of Phoenix fits files is erg/s/cm**2/cm
+            We transform the flux into photons in the read_spectrum()
+            function by multiplying the flux result by the wavelength (lambda)
 
-                spectra_micron = spectra * 10**-4              # Convert   /cm    to  /micron
+                Flux_photon = Flux_energy/Energy_photon
+            with
+                Energy_photon = h*c/lambda
+            Flux_photon = Flux_energy * lambda / (h * c)
 
-                if flux_type == "photon":
-                    wavelength_micron = wavelength * 10**-4    # Convert Angstrom to   micron
+            Here we convert the flux into erg/s/cm**2/\mum by multiplying by 10**-4 cm/\mum
+            Flux_e(erg/s/cm**2/\mum)  = Flux_e(erg/s/cm**2/cm) * (1 cm) / (10000 \mum)
+            """
 
-                    spectra_photon = spectra_micron * wavelength_micron  # Ignoring constants h*c in photon energy equation
+            spectra_micron = spectra * 10**-4              # Convert   /cm    to  /micron
 
-                    result = pdwrite_2col(output_filename, wavelength_micron, spectra_photon,
-                                          header=["# Wavelength (micron)", r"Flux (photon/s/cm^2)"])
+            if flux_type == "photon":
+                wavelength_micron = wavelength * 10**-4    # Convert Angstrom to   micron
 
-                else:
-                    result = pdwrite_2col(output_filename, wavelength, spectra_micron,
-                                          header=["# Wavelength (Angstom)", r"Flux (erg/s/cm^2/micron)"])
+                spectra_photon = spectra_micron * wavelength_micron  # Ignoring constants h*c in photon energy equation
 
-                if not result:
-                    print("Successfully wrote to ", output_filename)
-                else:
-                    print("Failed to write to ", output_filename)
+                result = pdwrite_2col(output_filename, wavelength_micron, spectra_photon,
+                                      header=["# Wavelength (micron)", r"Flux (photon/s/cm^2)"])
+
+            else:
+                result = pdwrite_2col(output_filename, wavelength, spectra_micron,
+                                      header=["# Wavelength (Angstom)", r"Flux (erg/s/cm^2/micron)"])
+
+            if not result:
+                print("Successfully wrote to ", output_filename)
+            else:
+                print("Failed to write to ", output_filename)
 
     print("Done")
     return 0
