56c3c0d00748037d8641b34784fc81769632b8c8
Jason Neal <jason.neal@astro.up.pt>
2017-01-24 16:53:06 +0000

    Convert spectral types into temperatures.
    Former-commit-id: d3490471d3674eed5b26c32ce332ef9833f3bf92 [formerly da46945877362b6851a2b11b2e1eb3c0c0c7b188] [formerly 64835aec098c6a5ecc5c9d743e7e2785a8025dff [formerly 4a76680c3c0f2c2c60caacc80e78a5fc63826bad]]
    Former-commit-id: d43b781ad040543a835221e41b49e4142b3ebcfe [formerly 4efe3b18467e60aa038ecf7f678d20363c907783]
    Former-commit-id: 710538ce76a10482a268f238cf589072e0519b1c

--- bin/prepare_data.py
+++ bin/prepare_data.py
@@ -65,6 +65,16 @@ def main(startype, temp, logg, metalicity, alpha, flux_type="photon", data_dir=N
     else:
         file_suffix = "_wave.dat"
 
+    # Convert Stellar_types into
+    stellar_dict = {"M0": 3900.0, "M3": 3500.0, "M6": 2800.0, "M9": 2600.0}
+    # Add temperature of stellar_type to temp list
+    for star in startype:
+        print(star)
+        try:
+            temp.append(stellar_dict[star])
+        except KeyError:
+            print("Stellar type {0} is not implemented here (yet), submit and issue.".format(star))
+
     # get all phoenix fits files we want to convert
     for (path, dirs, files) in os.walk(phoenix_dir):
         # print(path)
