3eab3e07392cd40f4d32c5c0935d475b4115ac1f
Jason Neal <jason.neal@astro.up.pt>
2018-02-22 13:31:19 +0000

    Also Extract other needed files.
    Former-commit-id: fea67aaba014df1335ffb42f409a2d8773c4fe44 [formerly cce6f81801f09a34d6676cc9e50181d21b532d5f] [formerly fbf3f54b2a57240ffcfd1d8c7e8ee5dc15e6cc87 [formerly fd79f129235d365cf8e13ec584286b1a5098d7dc]]
    Former-commit-id: 6abbbb49adbfdad75b2664492619475f2b336c5e [formerly f0b54b1b43b24a0080e91c8a9d818a627d1e4e49]
    Former-commit-id: 69b844bed6f7ac79736933a353bbdb69568d9e2e

---
+++ bin/mk_travis_data.py
@@ -0,0 +1,25 @@
+"""To be run on travis to generate the results data.
+
+Copare to published results and run other tests on it.
+Don't do to many.
+"""
+import os
+import subprocess
+
+subprocess.call("python bin/prepare_data.py -s M0 M3 M6 M9 -l 4.50 -m 0.0", shell=True)
+
+parameters = [("M0", "Z", 1, "60k"),
+              ("M0", "H", 1, "60k"),
+              ("M0", "Y", 10, "100k"),
+              ("M0", "K", 5, "60k 100k"),
+              ("M6", "H", 1, "80k"),
+              ("M9", "K", 5, "60k"),
+              ("M9", "H", 1, "100k"),
+              ("M6", "J", 10, "100k"),
+              ("M3", "Y", 5, "80k")]
+
+for sptype, band, vel, res in parameters:
+    subprocess.call(["python bin/nIR_run.py -s {0} -b {1} -R {2} -v {3}".format(sptype, band, res, vel)], shell=True)
+
+# List data files
+print([d for d in os.walk("data")])
