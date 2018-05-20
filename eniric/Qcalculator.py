9a48dd1a04ea1c839aa2bc95b3892a04a131b44a
Jason Neal <jason.neal@astro.up.pt>
2018-05-20 22:17:07 +0100

    fix typo

--- eniric/Qcalculator.py
+++ eniric/Qcalculator.py
@@ -112,7 +112,7 @@ def quality(wavelength: Union[Quantity, ndarray], flux: Union[Quantity, ndarray]
         flux = np.asarray(flux)
     
     flux = flux = u.dimensionless_unscaled # Turn into Quantity if not already
-    flux 0 flux / flux.unit  # Remove units from flux (sqrt(N_e) is unitless)
+    flux = flux / flux.unit  # Remove units from flux (sqrt(N_e) is unitless)
 
     wis = sqrt_sum_wis(wavelength, flux)
 
