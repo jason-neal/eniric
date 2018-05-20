e797a7f40b685a6c86c93ac13faddc84563b06b4
Jason Neal <jason.neal@astro.up.pt>
2018-05-20 22:22:43 +0100

    actual fix
    Former-commit-id: 620b270381ee70003d5f953919fa35479475107f [formerly b81f1764819248e89c673c9c3a2e30ea3a2477f0] [formerly 802704c5fe808ea2d9b7496d1dcdd5e097dd2699 [formerly 27ae69424b8e3220b251c819485853bff7189a46 [formerly 4abcd6a5c86236bc150cff8860c4da484e832ac2] [formerly bdb2489f3a947a663fa9d08630145712232ea8b1 [formerly 9557576b6d0fc8b94f40d4abd414550aacbf932d]]]]
    Former-commit-id: f7198313b50ff60efb452779548da63242d69696 [formerly 9311f007e93d8d13a2254692a5181c4672642c39]
    Former-commit-id: 6fbb4eb678eaa24d4d46389d3b026cf3191c494c
    Former-commit-id: a83192cedd765931f284e1c3642cc6ccd608afcc [formerly f8c03aacff2ff2b7e01668972b37ed54462b26e2]
    Former-commit-id: 3ce8edd4d30bcc95974916248144b6f0e37dc07c

--- eniric/Qcalculator.py
+++ eniric/Qcalculator.py
@@ -111,7 +111,7 @@ def quality(wavelength: Union[Quantity, ndarray], flux: Union[Quantity, ndarray]
     if not isinstance(flux, np.ndarray):
         flux = np.asarray(flux)
     
-    flux = flux = u.dimensionless_unscaled # Turn into Quantity if not already
+    flux = flux * u.dimensionless_unscaled # Turn into Quantity if not already
     flux = flux / flux.unit  # Remove units from flux (sqrt(N_e) is unitless)
 
     wis = sqrt_sum_wis(wavelength, flux)
