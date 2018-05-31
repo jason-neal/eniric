9dd4ad99032aa533203d64247376051666d83fa8
Jason Neal <jason.neal@astro.up.pt>
2018-05-31 18:42:44 +0100

    Testing previously published results.
    Former-commit-id: 5e16fc53ccd768269e6cf95ed7c6c24ef57f1143 [formerly 39d1244c7b0f81c4017f41c9f98288127c742c59] [formerly 343236d0204b4192aa97fc555675780dfd544fa7 [formerly 706b421c90f2bae42e95a496ece1e6df8a13633a [formerly 74b5a4ddb907d9f28d990907d46ffe405e3f4c9a] [formerly d29c5934fea43fb3dca3027b871eade1a867f0f3 [formerly e6d27228f251068b5b473e502a4048d1eba4f007]]]]
    Former-commit-id: 64b2ad830d4bda482969141fbfa30c6e0bd48694 [formerly e36b687b26146a5d3c43995ab0e8102f30d3391c]
    Former-commit-id: 5cb7cfd95af7c7d9adc5cada5eb0720b9e263046
    Former-commit-id: cb1a16fbe50f2c30a351a46167f6695fef43e6aa [formerly f27cc537e96f6466b99dd4357fa531bc69731312]
    Former-commit-id: 4509ca1ffbf3933d7d7bcd51798b4108853efe41

--- eniric_scripts/mk_travis_data.py
+++ eniric_scripts/mk_travis_data.py
@@ -21,19 +21,29 @@ parameters = [("M0", "Z", 1, "60k"),
               ("M9", "K", 5, "60k"),
               ("M9", "H", 1, "100k"),
               ("M6", "J", 10, "100k"),
-              ("M3", "Y", 5, "80k")]
+              ("M3", "Y", 5, "80k"),
+              (["M0", "M3", "M6", "M9"], "J", [1, 5, 10], ["60k", "80k", "100k"]),
+              ]
 
 counter = 0
 start_time = datetime.now()
 for sptype, band, vel, res in parameters:
-    # subprocess.call(["python eniric_scripts/nIR_run.py -s {0} -b {1} -R {2} -v {3}".format(sptype, band, res, vel)], shell=True)
+    # subprocess.call(["python eniric_scripts/nIR_run.py
+    #       -s {0} -b {1} -R {2} -v {3}".format(sptype, band, res, vel)], shell=True)
     if not isinstance(res, list):
         res = [res]
-    nir_run(startype=[sptype], vsini=[vel], resolution=res, band=[band])
+    if not isinstance(vel, list):
+        vel = [vel]
+    if not isinstance(band, list):
+        band = [band]
+    if not isinstance(sptype, list):
+        sptype = [sptype]
+
+    nir_run(startype=sptype, vsini=vel, resolution=res, band=band)
     counter += 1
 end_time = datetime.now()
 
 # List data files
 print([d for d in os.walk("data")])
 
-print("Preformed {} convolutions in {}".format(counter, end_time-start_time))
+print("Preformed {} convolutions in {}".format(counter, end_time - start_time))
--- tests/test_published_results.py
+++ tests/test_published_results.py
@@ -1,24 +1,43 @@
 """To test if the new code produces the same precision values on the published results."""
 
 import numpy as np
+import pandas as pd
 import pytest
 
 from eniric_scripts.nIR_precision import calculate_prec
 from eniric_scripts.prec_1 import calc_prec1
 
 
-path = "data/Published_Results/resampled/"
+@pytest.fixture
+def published_data():
+    name = "data/precision/precision_data_paper2015.txt"
+    df = pd.read_csv(name, sep="\t")
+    return df
 
 
-@pytest.mark.xfail(raises=FileNotFoundError)  # Data file may not exist
-def test_precision_1():
-    """New precision 1 test that works."""
-    published_results = {1: 3.8, 5: 9.1, 10: 20.7}
-    for vsini in [1, 5, 10]:
-        # name = "Spectrum_M0-PHOENIX-ACES_Yband_vsini{0}.0_R100k_res3.txt".format(vsini)
-        __, p1 = calc_prec1("M0", "Y", vsini, "100k", 3)
+@pytest.mark.parametrize("SpType,band,vsini,R", [
+    ("M0", "Z", 1, "60k"),
+    ("M0", "H", 1, "60k"),
+    ("M0", "Y", 10, "100k"),
+    ("M0", "K", 5, "60k"),
+    ("M0", "K", 5, "100k"),
+    ("M6", "H", 1, "80k"),
+    ("M9", "K", 5, "60k"),
+    ("M9", "H", 1, "100k"),
+    ("M6", "J", 10, "100k"),
+    ("M3", "Y", 5, "80k")
+])
+def test_precision_1(published_data, SpType, band, vsini, R):
+    """Test  calc_prec1 works. Values are not very different to published values."""
+    id_string, p1 = calc_prec1(SpType, band, vsini, R, 3)
 
-        assert abs(np.round(p1, 2).value - published_results[vsini]) < 0.5
+    published_precision_1 = published_data["RV_Cond_1[m/s]"][published_data.Simulation == id_string].values
+
+    # Absolute change
+    assert np.abs(np.round(p1.value, 2) - published_precision_1) < 0.4
+
+    # Fractional change
+    assert (np.abs(np.round(p1.value, 2) - published_precision_1) / published_precision_1) < 0.02
 
 
 @pytest.mark.parametrize("SpType,band,vsini,R,expected", [
@@ -38,3 +57,76 @@ def test_old_calc_precision(SpType, band, vsini, R, expected):
 
     # precision 3
     assert expected[2] == round(results[id_string][2].value, 1)
+
+
+@pytest.mark.parametrize("SpType,band,vsini,R", [
+    ("M0", "Z", 1, "60k"),
+    ("M0", "H", 1, "60k"),
+    ("M0", "Y", 10, "100k"),
+    ("M0", "K", 5, "60k"),
+    ("M0", "K", 5, "100k"),
+    ("M6", "H", 1, "80k"),
+    ("M9", "K", 5, "60k"),
+    ("M9", "H", 1, "100k"),
+    ("M6", "J", 10, "100k"),
+    ("M3", "Y", 5, "80k")
+])
+def test_published_precision_with_old_normalization(SpType, band, vsini, R, published_data):
+    # Testing calculate_prec with old normalization values are exactly the published values."""
+    id_string = "{0:s}-{1:s}-{2:.1f}-{3:s}".format(SpType, band, float(vsini), R)
+    results = calculate_prec([SpType], [band], [float(vsini)], [R], [3],
+                             plot_atm=False, plot_ste=False,
+                             plot_flux=False, paper_plots=False, rv_offset=0.0,
+                             use_unshifted=False, snr=100, ref_band="J", new=False)
+    print(published_data.head())
+
+    # Precision 1
+    published = published_data[published_data.Simulation == id_string]
+    assert published["RV_Cond_1[m/s]"].values == round(results[id_string][0].value, 1)
+
+    # precision 2 has changed
+    assert published["RV_Cond_1[m/s]"].values != round(results[id_string][2].value, 1)
+
+    # precision 3
+    assert published["RV_Cond_3[m/s]"].values == round(results[id_string][2].value, 1)
+
+
+@pytest.mark.parametrize("SpType,band,vsini,R", [
+    ("M0", "Z", 1, "60k"),
+    ("M0", "H", 1, "60k"),
+    ("M0", "Y", 10, "100k"),
+    ("M0", "K", 5, "60k"),
+    ("M0", "K", 5, "100k"),
+    ("M6", "H", 1, "80k"),
+    ("M9", "K", 5, "60k"),
+    ("M9", "H", 1, "100k"),
+    ("M6", "J", 10, "100k"),
+    ("M3", "Y", 5, "80k")
+])
+def test_published_precision_with_new_normalization(SpType, band, vsini, R, published_data):
+    # Testing calculate_prec with new normalization values are similar."""
+    id_string = "{0:s}-{1:s}-{2:.1f}-{3:s}".format(SpType, band, float(vsini), R)
+    results = calculate_prec([SpType], [band], [float(vsini)], [R], [3],
+                             plot_atm=False, plot_ste=False,
+                             plot_flux=False, paper_plots=False, rv_offset=0.0,
+                             use_unshifted=False, snr=100, ref_band="J", new=True)
+    published = published_data[published_data.Simulation == id_string]
+    print(published.head())
+
+    # Precision 1
+    published_prec_1 = published["RV_Cond_1[m/s]"].values
+    result_1 = round(results[id_string][0].value, 1)
+    assert np.abs(result_1 - published_prec_1) < 0.4
+    assert (np.abs(result_1 - published_prec_1) / published_prec_1) < 0.025
+
+    # precision 2 has changed
+    published_prec_2 = published["RV_Cond_2[m/s]"].values
+    result_2 = round(results[id_string][1].value, 1)
+    assert np.abs(result_2 - published_prec_2) > 1
+    # assert published["RV_Cond_1[m/s]"].values != round(results[id_string][2].value, 1)
+
+    # precision 3
+    published_prec_3 = published["RV_Cond_3[m/s]"].values
+    result_3 = round(results[id_string][2].value, 1)
+    assert np.abs(result_3 - published_prec_3) < 0.4
+    assert (np.abs(result_3 - published_prec_3) / published_prec_3) < 0.025
