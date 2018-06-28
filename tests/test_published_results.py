511271d1b040ee1d02e46103a8160f12d7e10829
Jason Neal <jason.neal@astro.up.pt>
2018-06-28 13:01:02 +0100

    Fixture for model parameters, Extend limits on closeness to old precision values.
    Former-commit-id: edb5007e3fd94c36e03033693f326533b053d72d [formerly a84c7865a44b21e0dd244dd9bad95f78d5ae5115] [formerly d104c4810ab49182deae4ced53191c92a1bc8d5c [formerly acf0e391ca637677ba665a22332bf42299d98232]]
    Former-commit-id: 14084cea4842a7939a9eac81b1e0f901992199f5 [formerly edf053649170308530f6612c1b980639c49e4fb0]
    Former-commit-id: 63597e37a4cdc9db7a4dd38a0824391787d6b872

--- tests/test_published_results.py
+++ tests/test_published_results.py
@@ -14,13 +14,13 @@ def published_data():
     return df
 
 
-
 @pytest.mark.parametrize("SpType,band,vsini,R,expected", [
-    ("M0", "Y", 10.0, "100k", [20.7, 21.4, 20.9]),
-    ("M9", "K", 5.0, "60k", [9.0, 10.1, 9.6]),
-    ("M6", "H", 1.0, "80k", [4.1, 4.4, 4.2])
+    ("M3", "K", 10.0, "100k", [26.1, 29.6, 27.9]),
+    ("M6", "H", 1.0, "80k", [4.1, 4.4, 4.2]),
+    ("M0", "K", 5.0, "60k", [20.7, 23.9, 22.1]),
 ])
 def test_old_calc_precision(SpType, band, vsini, R, expected):
+    """Testing some direct examples from Figueira et al. 2016"""
     id_string = "{0:s}-{1:s}-{2:.1f}-{3:s}".format(SpType, band, float(vsini), R)
     results = calculate_prec([SpType], [band], [float(vsini)], [R], [3],
                              plot_atm=False, plot_ste=False,
@@ -34,20 +34,9 @@ def test_old_calc_precision(SpType, band, vsini, R, expected):
     assert expected[2] == round(results[id_string][2].value, 1)
 
 
-@pytest.mark.parametrize("SpType,band,vsini,R", [
-    ("M0", "Z", 1, "60k"),
-    ("M0", "H", 1, "60k"),
-    ("M0", "Y", 10, "100k"),
-    ("M0", "K", 5, "60k"),
-    ("M0", "K", 5, "100k"),
-    ("M6", "H", 1, "80k"),
-    ("M9", "K", 5, "60k"),
-    ("M9", "H", 1, "100k"),
-    ("M6", "J", 10, "100k"),
-    ("M3", "Y", 5, "80k")
-])
-def test_published_precision_with_old_normalization(SpType, band, vsini, R, published_data):
+def test_published_precision_with_old_normalization(model_parameters, published_data):
     # Testing calculate_prec with old normalization values are exactly the published values."""
+    SpType, band, vsini, R = model_parameters
     id_string = "{0:s}-{1:s}-{2:.1f}-{3:s}".format(SpType, band, float(vsini), R)
     results = calculate_prec([SpType], [band], [float(vsini)], [R], [3],
                              plot_atm=False, plot_ste=False,
@@ -66,20 +55,9 @@ def test_published_precision_with_old_normalization(SpType, band, vsini, R, publ
     assert published["RV_Cond_3[m/s]"].values == round(results[id_string][2].value, 1)
 
 
-@pytest.mark.parametrize("SpType,band,vsini,R", [
-    ("M0", "Z", 1, "60k"),
-    ("M0", "H", 1, "60k"),
-    ("M0", "Y", 10, "100k"),
-    ("M0", "K", 5, "60k"),
-    ("M0", "K", 5, "100k"),
-    ("M6", "H", 1, "80k"),
-    ("M9", "K", 5, "60k"),
-    ("M9", "H", 1, "100k"),
-    ("M6", "J", 10, "100k"),
-    ("M3", "Y", 5, "80k")
-])
-def test_published_precision_with_new_normalization(SpType, band, vsini, R, published_data):
+def test_published_precision_with_new_normalization(model_parameters, published_data):
     # Testing calculate_prec with new normalization values are similar."""
+    SpType, band, vsini, R = model_parameters
     id_string = "{0:s}-{1:s}-{2:.1f}-{3:s}".format(SpType, band, float(vsini), R)
     results = calculate_prec([SpType], [band], [float(vsini)], [R], [3],
                              plot_atm=False, plot_ste=False,
@@ -91,8 +69,7 @@ def test_published_precision_with_new_normalization(SpType, band, vsini, R, publ
     # Precision 1
     published_prec_1 = published["RV_Cond_1[m/s]"].values
     result_1 = round(results[id_string][0].value, 1)
-    assert np.abs(result_1 - published_prec_1) < 0.4
-    assert (np.abs(result_1 - published_prec_1) / published_prec_1) < 0.025
+    assert (np.abs(result_1 - published_prec_1) / published_prec_1) < 0.05
 
     # precision 2 has changed
     published_prec_2 = published["RV_Cond_2[m/s]"].values
@@ -103,5 +80,29 @@ def test_published_precision_with_new_normalization(SpType, band, vsini, R, publ
     # precision 3
     published_prec_3 = published["RV_Cond_3[m/s]"].values
     result_3 = round(results[id_string][2].value, 1)
-    assert np.abs(result_3 - published_prec_3) < 0.4
-    assert (np.abs(result_3 - published_prec_3) / published_prec_3) < 0.025
+    assert (np.abs(result_3 - published_prec_3) / published_prec_3) < 0.05
+
+
+@pytest.fixture(params=[
+    ("M0", "Z", 1, "60k"),
+    ("M0", "Z", 1, "100k"),
+    ("M0", "Z", 10, "60k"),
+    ("M0", "Z", 10, "100k"),
+    ("M0", "H", 1, "60k"),
+    ("M0", "H", 1, "100k"),
+    ("M0", "H", 10, "60k"),
+    ("M0", "H", 10, "100k"),
+    ("M3", "Y", 10, "100k"),
+    ("M3", "K", 10, "100k"),
+    ("M0", "K", 1, "60k"),
+    ("M0", "K", 1, "100k"),
+    ("M0", "K", 5, "60k"),
+    ("M0", "K", 5, "100k"),
+    ("M6", "H", 1, "80k"),
+    ("M6", "K", 1, "80k"),
+    ("M9", "H", 1, "80k"),
+    ("M9", "K", 1, "80k"),
+])
+def model_parameters(request):
+    # Tuple of "SpType,band,vsini,R"
+    return request.param
