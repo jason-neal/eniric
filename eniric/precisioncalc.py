580a93ab4c41706b6ab138853ec3c6315a821be3
Jason Neal <jason.neal@astro.up.pt>
2018-08-21 00:07:51 +0100

    Remove some obsolete tests and methods of rv class etc.

--- eniric/precisioncalc.py
+++ eniric/precisioncalc.py
@@ -1,12 +1,12 @@
-import warnings
-
-from typing import Any, List, Optional, Tuple, Union
+from typing import Optional, Union
 
 import astropy.units as u
 import numpy as np
-from astropy.constants import c
+from astropy import constants as const
 from astropy.units.quantity import Quantity
-from numpy import float64, ndarray
+from numpy import ndarray
+
+c = const.c
 
 
 class RvPrecision(object):
@@ -42,24 +42,31 @@ class RvPrecision(object):
 
     """
 
-    def __init__(self,
-                 wavelength: Union[Quantity, ndarray],
-                 flux: Union[Quantity, ndarray],
-                 mask: Optional[ndarray] = None,
-                 telluric: Optional[ndarray] = None,
-                 ):
+    def __init__(
+        self,
+        wavelength: Union[Quantity, ndarray],
+        flux: Union[Quantity, ndarray],
+        mask: Optional[ndarray] = None,
+        telluric: Optional[ndarray] = None,
+        grad=True,
+    ):
         if mask is None:
             mask = np.ones_like(flux, dtype=np.bool)
         if telluric is None:
             telluric = np.ones_like(flux, dtype=np.bool)
 
-        if len(wavelength) != len(flux) or (len(flux) != len(mask)) or (len(flux) != len(telluric)):
+        if (
+            len(wavelength) != len(flux)
+            or (len(flux) != len(mask))
+            or (len(flux) != len(telluric))
+        ):
             raise ValueError("Input values are not the correct")
 
         self.wavelength = wavelength
         self.flux = flux
         self.mask = mask
         self.telluric = telluric
+        self.grad = grad
 
     def pixel_weights(self, mask=None):
         """Optimal pixel weights
@@ -94,8 +101,10 @@ class RvPrecision(object):
         else:
             flux_variance = self.flux
 
-        weigths = self.wavelength[:-1] ** 2.0 * derivf_over_lambda ** 2.0 / flux_variance[:-1]
-        return weigths * mask[:-1] ** 2
+        weights = (
+            self.wavelength[:-1] ** 2.0 * derivf_over_lambda ** 2.0 / flux_variance[:-1]
+        )
+        return weights * mask[:-1] ** 2
 
     def sqrt_sum_weights(self, mask=None):
         """Square root of sum of pixel weights."""
@@ -116,65 +125,15 @@ class RvPrecision(object):
         print("Check if the spectra quality need mask function applied.")
         return self.sqrt_sum_weights(mask=None) / np.sqrt(self.total_flux())
 
-    def rv_prec(self):
+    def condition1(self):
         """Precision using pixel weights without masking."""
         return c / self.sqrt_sum_weights(mask=None)
 
-    def rv_prec_mask(self):
-        """Using mask directly on pixel weights."""
-        # masks should be 1/or zero only
-        assert np.all((self.mask == 0) | (self.mask == 1))  # all values are 1 or zero
-        #tweaked mask   account for increased pixel loss due to multiple diffs when clumping
-        mask = self.mask.copy()
-        print("mask ratio", "{}/{}".format(sum(mask),len(mask)))
-        mask[:-1] = mask[:-1]*mask[1:]
-        assert not np.allclose(self.mask, mask)
-        print("new mask ratio", "{}/{}".format(sum(mask),len(mask)))
+    def condition2(self):
         return c / self.sqrt_sum_weights(mask=self.mask)
 
-    def rv_prec_clumped(self):
-        # return c / self.sqrt_sum_weights(mask=False)
-        from eniric.Qcalculator import mask_clumping
-        wavelength_clumps, flux_clumps = mask_clumping(self.wavelength, self.flux, self.mask)
-
-        slice_rvs = Quantity(
-            np.zeros(len(wavelength_clumps), dtype=float), unit=u.meter / u.second
-        )  # Radial velocity of each slice
-
-        for i, (wav_slice, flux_slice) in enumerate(zip(wavelength_clumps, flux_clumps)):
-            if len(wav_slice) == 1:
-                """Results in infinite rv, can not determine the slope of single point."""
-                continue
-            else:
-                wav_slice = np.asarray(wav_slice)
-                flux_slice = np.asarray(flux_slice)
-                slice_rvprec = RvPrecision(wav_slice, flux_slice)
-                slice_rvs[i] = slice_rvprec.rv_prec()
-
-        # Zeros created from the initial empty array, when skipping single element chunks)
-        slice_rvs = slice_rvs[np.nonzero(slice_rvs)]  # Only use nonzero values.
-        slice_rvs = slice_rvs[np.isfinite(slice_rvs)]
-        return self.weighted_error(slice_rvs)
-
-    def rv_prec_trans(self):
-        """The same as RV_prec_calc, but considering a transmission different than zero.
-
-            Parameters
-            ----------
-            wavelength: array-like or Quantity array
-                Wavelength array
-            flux: array-like or Quantity array
-                Flux array
-            transmission: array-like
-                Transmission array
-
-            Returns
-            -------
-            RVrms: Quantity scalar
-                Radial velocity precision for a spectrum affected by atmospheric transmission
-            """
+    def condition3(self):
         return c / self.sqrt_sum_weights(mask=self.telluric)
-
 
     @staticmethod
     def weighted_error(rv_errors):
@@ -183,26 +142,3 @@ class RvPrecision(object):
         rv_value = 1.0 / (np.sqrt(np.sum((1.0 / rv_errors) ** 2.0)))
 
         return rv_value
-
-    def rv_shift_mask(self, rv=30):
-        """Extend transmission mask.
-        RV shift the telluric spectra +- 30km/s"""
-        #See how it is done in atmosphere
-        # TODO
-        # assert False
-
-    def mask_deep_telluric(self, percent=2):
-        """Mask out telluric lines deeper than given percent.
-
-        Parameters
-        ----------
-        percent: float  (default 2)
-            Depth percentage limit of telluric lines to mask.
-
-        Transforms self.mask from a transmission spectrum to a boolean mask.
-        Idempotent if already a boolean mask.
-        """
-        if np.all(self.telluric == 1):
-            warnings.warn("Telluric us all unity, have you added a telluric spectra.")
-
-        self.mask = self.telluric >= (1 - percent / 100.0)
--- tests/test_precisioncalc.py
+++ tests/test_precisioncalc.py
@@ -4,12 +4,7 @@ from astropy.constants import c
 from hypothesis import assume, given, strategies as st
 
 from eniric.precisioncalc import RvPrecision
-from eniric.Qcalculator import (
-    RV_prec_calc_Trans,
-    RVprec_calc,
-    RVprec_calc_masked,
-    quality as qual,
-)
+from eniric.Qcalculator import RVprec_calc, quality as qual
 
 
 @pytest.mark.parametrize("l1, l2, l3", [(2, 2, 2), (60, 60, 60), (30, 30, None)])
@@ -71,40 +66,10 @@ def test_rv1_prec_consistency(wav_flux):
     flux = np.array(wav_flux[0])
 
     rv_prec = RvPrecision(wavelength, flux)
-    old_rv = RVprec_calc(wavelength, flux)
-
-    assert old_rv == rv_prec.rv_prec()
-
-
-@given(
-    st.integers(min_value=10, max_value=100).flatmap(
-        lambda n: st.lists(
-            st.lists(st.floats(min_value=0.5, max_value=5000), min_size=n, max_size=n),
-            min_size=3,
-            max_size=3,
-        )
-    )
-)
-# @given(wav=st.lists(st.floats(min_value=0.5, max_value=5000), min_size=2, max_size=500),
-#       flux=st.lists(st.floats(min_value=0, max_value=5000), min_size=2, max_size=500),
-#       mask= st.lists(st.integers(min_value=0, max_value=1), min_size=2, max_size=500))
-def test_rv2_prec_clumped_consistency(wav_flux_mask):
-
-    wav = np.array(wav_flux_mask[0])
-    wav.sort()
-    assume(np.all(np.diff(wav) > 0.000005))
-    flux = np.array(wav_flux_mask[1])
-    mask = wav_flux_mask[2]
-    mask = np.array(mask) > np.mean(mask)
-    assume(sum(mask) > len(mask) / 4)
-
-    rv_prec = RvPrecision(wav, flux, mask)
-    print(mask, np.max(mask), np.min(mask))
-    rv2 = rv_prec.rv_prec_clumped()
-    rv2_old = RVprec_calc_masked(wav, flux, mask)
-    rv2_ = rv_prec.rv_prec_mask()
-    assert rv2 == rv2_old
-    assert rv2 == rv2_
+    old_rv = RVprec_calc(wavelength, flux, grad=False)
+    rv_class_cond1 = rv_prec.condition1()
+    assert np.allclose(old_rv.value, rv_class_cond1.value)
+    assert old_rv.unit == rv_class_cond1.unit
 
 
 @given(
@@ -128,8 +93,8 @@ def test_rv2_prec_consistency(wav_flux_mask):
 
     rv_prec = RvPrecision(wav, flux, mask)
     print(mask, np.max(mask), np.min(mask))
-    rv2 = rv_prec.rv_prec_mask()
-    rv2_old = RVprec_calc_masked(wav, flux, mask)
+    rv2 = rv_prec.condition2()
+    rv2_old = RVprec_calc(wav, flux, mask)
 
     assert rv2 == rv2_old
 
@@ -156,45 +121,9 @@ def test_rv3_prec_consistency(wav_flux_tell):
 
     rv_prec = RvPrecision(wav, flux, telluric=telluric)
 
-    rv3 = rv_prec.rv_prec_trans()
-    rv3_old = RV_prec_calc_Trans(wav, flux, telluric)
+    rv3 = rv_prec.condition3()
+    rv3_old = RVprec_calc(wav, flux, telluric)
     assert rv3 == rv3_old
-
-
-def test_pixel_mask_verse_clump():
-    length = 500
-    wavelength = np.random.randn(length) + 1.1
-    flux = np.random.randn(length) + 1
-    mask = np.random.randint(0, 1, length)
-    rv_prec = RvPrecision(wavelength, flux, mask)
-
-    rv2_mask = rv_prec.rv_prec_mask()
-    rv2_clump = rv_prec.rv_prec_clumped()
-    print(rv2_mask, "masked")
-    print(rv2_clump, "clumped")
-
-    assert rv2_mask == rv2_clump
-
-
-@pytest.mark.parametrize("percentage", [2, 5])
-def test_mask_deep_telluric(resampled_data, percentage):
-    """Test telluric masking."""
-    id, wav, flux = resampled_data
-    tell = flux / np.max(flux)
-
-    # Check tell between 0 and 1, and there are some deep telluric lines.
-    assert np.all((tell <= 1) | (tell >= 0))
-    assert not np.all(tell >= (1 - percentage / 100))
-
-    # Initialize class
-    rv = RvPrecision(wav, flux, telluric=tell)
-
-    # Check initial mask
-    assert np.all(rv.mask == 1)
-    assert not np.all(rv.telluric[rv.mask] >= (1 - percentage / 100))
-
-    rv.mask_deep_telluric(percentage)
-    assert np.all(rv.telluric[rv.mask] >= (1 - percentage / 100))
 
 
 def test_quality_same(resampled_data):
@@ -217,14 +146,13 @@ def test_rv_precision_functional_test(resampled_data):
     id_string, wav, flux = resampled_data
 
     # Obtain the telluric mask spectra
-    from eniric_scripts.aces_precision import get_corresponding_atm
+    from eniric.atmosphere import Atmosphere
 
-    wav_atm, flux_atm, mask_atm = get_corresponding_atm(
-        wav, id_string.split("-")[1], bary=True
-    )
+    atm = Atmosphere.from_band(id_string.split("-")[1], bary=True)
+    atm = atm.at(wav)
 
     # Add to RvPrecision object
-    rv_prec = RvPrecision(wav, flux, mask=mask_atm, telluric=flux_atm)
+    rv_prec = RvPrecision(wav, flux, mask=atm.mask, telluric=atm.transmission)
 
     # Calculate spectral quality
     quality = rv_prec.Q()
@@ -239,20 +167,16 @@ def test_rv_precision_functional_test(resampled_data):
 
     # rv3
     rv3 = rv_prec.rv_prec_trans()
-    rv3_old = RV_prec_calc_Trans(wav, flux, flux_atm)
+    rv3_old = RVprec_calc(wav, flux, atm.transmission)
     print("rv3", rv3)
     print("rv3_old", rv3_old)
 
     # Calculate the precision from masking
     rv2 = rv_prec.rv_prec_mask()
     print("rv2", rv2)
-    print("rv2 old", RVprec_calc_masked(wav, flux, mask=mask_atm))
-
-    # Check that it is the same as clumped masking
-    rv2_clumped = rv_prec.rv_prec_clumped()
+    print("rv2 old", RVprec_calc(wav, flux, mask=atm.mask))
 
     assert (rv1 < rv3) and (rv3 < rv2)
-    assert rv2 == rv2_clumped
 
 
 # @pytest.fixture()
