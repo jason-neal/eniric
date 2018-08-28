0719caa32e432740eb4188f29c66cd7a37adf2a3
Jason Neal <jason.neal@astro.up.pt>
2018-08-28 11:17:46 +0100

    Update setup.py/bump version

--- setup.py
+++ setup.py
@@ -9,18 +9,14 @@ config = {
     'url': 'https://github.com/jason-neal/eniric.git',
     'download_url': 'https://github.com/jason-neal/eniric.git',
     'author_email': 'jason.neal@astro.up.pt',
-    'version': '0.5',
+    'version': '0.5.1',
     'license': 'MIT',
     'setup_requires': ['pytest-runner'],
     'install_requires': ['pytest'],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     'extras_require': {
         'dev': ['check-manifest'],
         'test': ['coverage', 'pytest', 'pytest-cov', 'python-coveralls', 'hypothesis'],
-    },
+    },  # $ pip install -e .[dev,test]
     'packages': ['eniric', 'eniric_scripts'],
     'scripts': ["eniric_scripts/make_test_data.py",
                 "eniric_scripts/any_spectral_quality.py",
@@ -31,26 +27,22 @@ config = {
     'name': 'eniric',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     "classifiers": [
-        # How mature is this project? Common values are
-        #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-        # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Scientific/Engineering :: Physics',
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Natural Language :: English',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
     ],
     # What does your project relate to?
-    "keywords": ['Astronomy', 'Near-infrared spectroscopy', "M-dwarfs"],
+    "keywords": ['Astronomy', 'Radial velocity', 'Near-infrared spectroscopy', "M-dwarfs"],
 }
 
 setup(**config)
