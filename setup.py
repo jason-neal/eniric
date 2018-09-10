f8a34317f94711da3c744d6db7cad1dd4aacd530
Jason Neal <jason.neal@astro.up.pt>
2018-09-10 19:09:42 +0100

    Fix classifier in setup.py
    Former-commit-id: 5e27edaef0290e1c3f880e70d425bc2e5ace4b7f [formerly 2ab7ce63c55e6781add5c5c63d59fb7997593338] [formerly 9f758147cc3b5736576148beed9702bf01620d21 [formerly cfc3324a6ec19d71fd1388b1ba8d9bdc65145541 [formerly 0719788542111d8c836b44f5deeddc1e7390336e] [formerly 5c6848e06a08acd134114668446fb67d59ca432f [formerly 8f3b90ca76f38a45f0b971f864d6900fbb4a1b0a]]]]
    Former-commit-id: 343ed9a52ec2012d15386d14387390fbb9a22e6e [formerly 099ec26ebdc511cca373c42fb1c40a8a83bd5e44]
    Former-commit-id: 9d6d696ba5e0b2db9616a8eb08c36f3f4d2b8139
    Former-commit-id: 0190d9806b09bb14c930ddf780d9559306940ac4 [formerly 24159157b6e08d6f5d99c9f45c6c7100efbf58d9]
    Former-commit-id: c5f578806fca753a1d61481e890564e228366db2

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ config = {
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
-        '"Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.7",
         "Natural Language :: English",
         "Operating System :: Unix",
         "Operating System :: Microsoft :: Windows",
