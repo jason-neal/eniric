8480bd3d2e8862e9d85f4cb09b47fe8d587a2fc4
Jason Neal <jason.neal@astro.up.pt>
2018-09-12 20:24:08 +0100

    Bump version 1.0.0rc2
    Former-commit-id: db0f17abd542062551b640402109a6e3165ab2b3 [formerly 24382f56bc0ffa0945f9e77854e43017772142b5]
    Former-commit-id: 363ac372749f4439bad4fd969d6c38eed25dfca0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ config = {
     "url": "https://github.com/jason-neal/eniric.git",
     "download_url": "https://github.com/jason-neal/eniric.git",
     "author_email": "jason.neal@astro.up.pt",
-    "version": "1.0rc1",
+    "version": "1.0.0rc2",
     "license": "MIT",
     "setup_requires": ["pytest-runner"],
     "tests_require": ["pytest", "hypothesis"],
