42bf378ba1380d694acd6a855114df4d7dc5e28b
Jason Neal <jason.neal@astro.up.pt>
2018-09-21 14:21:10 +0100

    csv2tsv.py utility
    Former-commit-id: 247ffb6cdc1f990ef0cb1076db21df76d6ebd98c [formerly 18304a47ae95bd2525a51b2f4a9df726e55ae84d]
    Former-commit-id: e1547fbcd63f93447e5d638d0ffb611d19d5b117

---
+++ eniric_scripts/csv2tsv.py
@@ -0,0 +1,11 @@
+import sys
+import pandas
+
+"""Simple script using pandas to transform from *.csv to *.tsv"""
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+
+    df = pandas.read_csv(name, header=0)
+
+    df.to_csv(name.replace(".csv", ".tsv"), sep="\t", header=True, index=False)
