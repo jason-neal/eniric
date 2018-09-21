1cfe5fe194e60f6438594b055256da0dfddbfb42
Jason Neal <jason.neal@astro.up.pt>
2018-09-21 10:17:45 +0100

    Update download_eniric_data.sh
    Avoid redirects from dropbox by requesting correct "raw" location
    
    Former-commit-id: aa1cdb679f52b308ecdb2071de9fb81018ae355e [formerly b0ef92b4ef8c0a7aebafeb51f7e83a17f61c5514]
    Former-commit-id: 4a382339776c7783889245a75c2fc1ad546aee5e

--- eniric_scripts/download_eniric_data.sh
+++ eniric_scripts/download_eniric_data.sh
@@ -14,7 +14,7 @@ if [ ! -d "$DIRECTORY" ]; then
   mkdir -p $DIRECTORY
 fi
 (cd $DIRECTORY
-    wget "https://www.dropbox.com/s/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz"
+    wget "https://www.dropbox.com/s/raw/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz"
     ../../eniric_scripts/untar_here.py Average_TAPAS_2014.dat.tar.gz
 )
 
@@ -27,7 +27,7 @@ if [ ! -d "$DIRECTORY2" ]; then
 fi
 
 (cd $DIRECTORY2
-    wget "https://www.dropbox.com/s/oq2x7dsjeuxrf7t/obsolete.tar.gz"
+    wget "https://www.dropbox.com/s/raw/oq2x7dsjeuxrf7t/obsolete.tar.gz"
     ../../eniric_scripts/untar_here.py obsolete.tar.gz
     rm obsolete.tar.gz
 )
@@ -43,7 +43,7 @@ if [ ! -d "$DIRECTORY3" ]; then
 fi
 
 (cd $DIRECTORY3
-    wget "https://www.dropbox.com/s/i4cxjrhcbx6e37x/precision.tar.gz"
+    wget "https://www.dropbox.com/s/raw/i4cxjrhcbx6e37x/precision.tar.gz"
    ../eniric_scripts/untar_here.py precision.tar.gz
     rm precision.tar.gz
 )
