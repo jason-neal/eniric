a160a541f218a78fcd2a9a79f81c2e9e773f9f0f
Jason Neal <jason.neal@astro.up.pt>
2018-09-24 17:36:32 +0100

    Move phoenix data to dropbox also. Testing was fialing due to phoenix data ftp.
    Former-commit-id: 86e6d44fbb0f0dddfdfaa6f555160ef04b2438b1 [formerly f69f1e0d51b2e209a45bc561086edeee291f4092]
    Former-commit-id: d76f61c2637983ba54d36aa6ca52b84b5de209a5

--- eniric_scripts/ps_download_eniric_data.ps1
+++ eniric_scripts/ps_download_eniric_data.ps1
@@ -37,32 +37,41 @@ Write-Host "$DIRECTORY is not present. Creating"
 mkdir $DIRECTORY
 }
 
+
 cd $DIRECTORY
 dir
-# Phoenix-raw data 
-wget "https://www.dropbox.com/s/raw/09fqard9l5s87zo/phoenix-raw.tar.gz" -UseBasicParsing -OutFile "phoenix-raw.tar.gz"
-dir
-python ..\..\eniric_scripts\untar_here.py ".\phoenix-raw.tar.gz"
-del "phoenix-raw.tar.gz"
-# obsolete data 
+# obsolete data
 wget "https://www.dropbox.com/s/raw/oq2x7dsjeuxrf7t/obsolete.tar.gz" -UseBasicParsing -OutFile "obsolete.tar.gz"
 python ..\..\eniric_scripts\untar_here.py ".\obsolete.tar.gz"
 del "obsolete.tar.gz"
 
+# Phoenix-raw data  and BT-settl raw data
+wget "https://www.dropbox.com/s/raw/skg8zwi7vnxgesj/data_raw.tar.gz" -UseBasicParsing -OutFile "phoenix-raw.tar.gz"
+dir
+python ..\..\eniric_scripts\untar_here.py ".\data_raw.tar.gz"
+del "data_raw.tar.gz"
+
 cd $HOMEDIR
 
-
-# BTsettl
-
-$DIRECTORY = ".\data\test_data\btsettl-raw\"
-
-Write-Host $DIRECTORY
-
-if ( -not (Test-Path $DIRECTORY -PathType Container))
-{
-Write-Host "$DIRECTORY is not present. Creating"
-mkdir $DIRECTORY
-}
-cd $DIRECTORY
-wget https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011_2015/FITS/lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz -UseBasicParsing -OutFile "lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz"
-cd $HOMEDIR
+# # Phoenix-raw data
+# wget "https://www.dropbox.com/s/raw/09fqard9l5s87zo/phoenix-raw.tar.gz" -UseBasicParsing -OutFile "phoenix-raw.tar.gz"
+# dir
+# python ..\..\eniric_scripts\untar_here.py ".\phoenix-raw.tar.gz"
+# del "phoenix-raw.tar.gz"
+#
+# cd $HOMEDIR
+#
+# # BTsettl
+#
+# $DIRECTORY = ".\data\test_data\btsettl-raw\"
+#
+# Write-Host $DIRECTORY
+#
+# if ( -not (Test-Path $DIRECTORY -PathType Container))
+# {
+# Write-Host "$DIRECTORY is not present. Creating"
+# mkdir $DIRECTORY
+# }
+# cd $DIRECTORY
+# wget https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011_2015/FITS/lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz -UseBasicParsing -OutFile "lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz"
+# cd $HOMEDIR
