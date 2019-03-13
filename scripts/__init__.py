31eb55223f48c6cebfe751b5ac95bbf5e710f116
Jason Neal <jason.neal@astro.up.pt>
2019-03-13 20:19:54 +1300

    adjust scripts/__init__.py

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,6 +1,8 @@
 __all__ = [
-    "barycenter_broaden_atmmodel.py",
+    "barycenter_broaden_atmmodel",
+    "csv2tsv",
     "phoenix_precision",
     "precision_four_panel",
     "split_atmmodel",
+    "untar_here",
 ]
