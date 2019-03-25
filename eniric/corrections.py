4b9c025da908c09f23f6d33dd5d36183c3b1c96f
Jason Neal <jason.neal@astro.up.pt>
2019-03-26 00:42:32 +1300

    Add corrections to theoretical precision docs

--- eniric/corrections.py
+++ eniric/corrections.py
@@ -1,10 +1,7 @@
-"""Module with functions that have precision corrections."""
+"""
+Table 3 from Artigau et al. (2018):
 
-
-def correct_artigau_2018(band: str) -> float:
-    """Apply Artigau 2018 nominal Barnard's Star corrections.
-
-    Table 3: Multiplicative correction factors to be applied
+    Multiplicative correction factors to be applied
     on the RV precision derived from stellar models.
     These values correspond to the square-root of
     the flux-weighted mean Q ratio between observation
@@ -13,22 +10,52 @@ def correct_artigau_2018(band: str) -> float:
     but we also explore the impact of other physical parameter
     choices.
 
-             |Nominal|  Variants
-    [Fe/H]   |−0.5   |  0.0
-    log g    | 5.0   |        5.5
-    Teff (K) | 3200  |              3400    3000
-    ----------------------------------------
-    g        |  0.66 | 0.63  0.69  0.82   0.51
-    r        |  0.82 | 0.76  0.84  1.08   0.60
-    i        |  0.94 | 0.81  0.99  1.26   0.73
-    z        |  1.27 | 1.09  1.20  1.82   0.96
-    Y        |  0.29 | 0.30  0.27  0.30   0.25
-    J        |  0.38 | 0.40  0.31  0.54   0.37
-    H        |  1.37 | 0.95  1.82  1.42   1.23
-    K        |  1.47 | 1.06  2.00  1.66   1.27
++---------+--------+-----+-----+-----+-----+
+|         |Nominal |        Variants       |
++=========+========+=====+=====+=====+=====+
+|[Fe/H]   |−0.5    |0.0  |     |     |     |
++---------+--------+-----+-----+-----+-----+
+|logg     |5.0     |     |5.5  |     |     |
++---------+--------+-----+-----+-----+-----+
+|Teff (K) |3200    |     |     |3400 |3000 |
++---------+--------+-----+-----+-----+-----+
+|g        |0.66    |0.63 |0.69 |0.82 |0.51 |
++---------+--------+-----+-----+-----+-----+
+|r        |0.82    |0.76 |0.84 |1.08 |0.60 |
++---------+--------+-----+-----+-----+-----+
+|i        |0.94    |0.81 |0.99 |1.26 |0.73 |
++---------+--------+-----+-----+-----+-----+
+|z        |1.27    |1.09 |1.20 |1.82 |0.96 |
++---------+--------+-----+-----+-----+-----+
+|Y        |0.29    |0.30 |0.27 |0.30 |0.25 |
++---------+--------+-----+-----+-----+-----+
+|J        |0.38    |0.40 |0.31 |0.54 |0.37 |
++---------+--------+-----+-----+-----+-----+
+|H        |1.37    |0.95 |1.82 |1.42 |1.23 |
++---------+--------+-----+-----+-----+-----+
+|K        |1.47    |1.06 |2.00 |1.66 |1.27 |
++---------+--------+-----+-----+-----+-----+
 
-    Usage:
-      Multiply by model precisions to get to "real" values.
+"""
+
+
+def correct_artigau_2018(band: str) -> float:
+    """Apply Artigau et al. (2018) Table 3 nominal Barnard's Star corrections.
+
+    Parameters
+    ----------
+    band: str
+       Wavelength band.
+
+    Returns
+    -------
+    correction: float
+       Correction value to multiply synthetic model precisions by to get to "real" values.
+
+    Warning
+    -------
+    Returns the corrections from the nominal (second) column only.
+
     """
 
     corrections = {
