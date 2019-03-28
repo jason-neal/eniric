c9a838b718d322516943cf2a752c36adda8e9418
Jason Neal <jason.neal@astro.up.pt>
2019-03-28 15:58:43 +1300

    Tidy up the .sh and .ps1 download scripts.
    Former-commit-id: 1a54c8e8c68383711ada65b809ee8b23e633465b [formerly eec66e1b1bf808ef04d9cb880cb95ccf33afb93d]
    Former-commit-id: acc30841bbb2933ead4738e154547ec85248b9cf

--- scripts/download/download_eniric_data.sh
+++ scripts/download/download_eniric_data.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Inspired by Starfish
-# Invoke this script from the top directory.
-
+# Invoke this script from the top eniric directory.
 
 # Check for an --no-check-certificates argument
 
@@ -41,50 +40,49 @@ else
     exit 1
  fi
 
-### DOWNLOAD Transmission spectrum
-DIRECTORY="data/atmmodel/"
 
-# Check to see if libraries/raw/ directory exists, if not, make it.
-if [ ! -d "$DIRECTORY" ]; then
-  echo $DIRECTORY does not exist, creating.
-  mkdir -p $DIRECTORY
+DIRECTORY1="data/"
+DIRECTORY2="data/atmmodel/"
+DIRECTORY3="tests/data/phoenix-raw/"
+DIRECTORY4="tests/data/btsettl-raw/"
+
+
+# Precision data
+if [ ! -d "$DIRECTORY1" ]; then
+  echo $DIRECTORY1 does not exist, creating.
+  mkdir -p $DIRECTORY1
 fi
-(cd $DIRECTORY
-    wget $certcheck "https://www.dropbox.com/s/raw/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz"
-    ../../scripts/untar_here.py Average_TAPAS_2014.dat.tar.gz
-)
-
-
-DIRECTORY2="tests/data/"
-# Check to see if libraries/raw/ directory exists, if not, make it.
-if [ ! -d "$DIRECTORY2" ]; then
-  echo $DIRECTORY2 does not exist, creating.
-  mkdir -p $DIRECTORY2
-fi
-
-
-DIRECTORY3="data/"
-
-# Check to see if libraries/raw/ directory exists, if not, make it.
-if [ ! -d "$DIRECTORY3" ]; then
-  echo $DIRECTORY2 does not exist, creating.
-  mkdir -p $DIRECTORY3
-fi
-
-(cd $DIRECTORY3
+(cd $DIRECTORY1
     wget $certcheck "https://www.dropbox.com/s/raw/i4cxjrhcbx6e37x/precision.tar.gz"
    ../scripts/untar_here.py precision.tar.gz
     rm precision.tar.gz
 )
 
+# Atmmodel data
+if [ ! -d "$DIRECTORY2" ]; then
+  echo $DIRECTORY2 does not exist, creating.
+  mkdir -p $DIRECTORY2
+fi
+(cd $DIRECTORY2
+    wget $certcheck "https://www.dropbox.com/s/raw/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz"
+    ../../scripts/untar_here.py Average_TAPAS_2014.dat.tar.gz
+)
 
-# This should download the PHOENIX spectra
-download_test_PHOENIX_spec.sh
+# ACES test data
+if [ ! -d "$DIRECTORY3" ]; then
+  echo $DIRECTORY3 does not exist, creating.
+  mkdir -p $DIRECTORY3
+fi
+# This should download the PHOENIX ACES spectra
+download_test_aces.py
 
-# FTP on travis gives several delays when getting phoenix data.
-# So putting Phoenix data in dropbox now also.
-#(cd $DIRECTORY2
-#    wget $certcheck "https://www.dropbox.com/s/raw/skg8zwi7vnxgesj/data_raw.tar.gz"
-#    ../../scripts/untar_here.py data_raw.tar.gz
-#    rm data_raw.tar.gz
-#)
+
+# BTsettl test data
+if [ ! -d "$DIRECTORY4" ]; then
+  echo $DIRECTORY4 does not exist, creating.
+  mkdir -p $DIRECTORY4
+fi
+(
+  cd $DIRECTORY4
+  wget $certcheck https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011_2015/FITS/lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz
+)
