75f412559ba973b9217237f224438840c30afde8
Jason Neal <jason.neal@astro.up.pt>
2019-03-28 15:58:43 +1300

    Tidy up the .sh and .ps1 download scripts.

--- scripts/download/ps_download_eniric_data.ps1
+++ scripts/download/ps_download_eniric_data.ps1
@@ -1,73 +1,59 @@
 # Script to Download the data needed for Eniric on Windows.
 
+# Invoke this script from the top eniric directory.
+
 $HOMEDIR = Convert-Path "."
 
-# Atmmodel data
-
-$DIRECTORY = ".\data\atmmodel\"
-
-Write-Host $DIRECTORY
-
-if ( -not (Test-Path $DIRECTORY -PathType Container))
-{
-Write-Host "$DIRECTORY is not present. Creating"
-mkdir $DIRECTORY
-}
-
-wget "https://www.dropbox.com/s/raw/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz" -UseBasicParsing -OutFile ".\data\atmmodel\Average_TAPAS_2014.dat.tar.gz"
+$DIRECTORY1 = ".\data\"
+$DIRECTORY2 = ".\data\atmmodel\"
+$DIRECTORY3 = ".\tests\data\phoenix-raw\"
+$DIRECTORY4 = ".\tests\data\btsettl-raw\"
 
 
 # Precision data
-cd ".\data\"
+Write-Host $DIRECTORY1
+if ( -not (Test-Path $DIRECTORY1 -PathType Container))
+{
+Write-Host "$DIRECTORY1 is not present. Creating"
+mkdir $DIRECTORY1
+}
+cd $DIRECTORY1
 wget "https://www.dropbox.com/s/raw/i4cxjrhcbx6e37x/precision.tar.gz" -UseBasicParsing -OutFile ".\precision.tar.gz"
 dir
 python ..\scripts\untar_here.py "precision.tar.gz"
 cd $HOMEDIR
 
 
-# Test data
-
-$DIRECTORY = ".\tests\data\"
-
-Write-Host $DIRECTORY
-
-if ( -not (Test-Path $DIRECTORY -PathType Container))
+# Atmmodel data
+Write-Host $DIRECTORY2
+if ( -not (Test-Path $DIRECTORY2 -PathType Container))
 {
-Write-Host "$DIRECTORY is not present. Creating"
-mkdir $DIRECTORY
+Write-Host "$DIRECTORY2 is not present. Creating"
+mkdir $DIRECTORY2
 }
-
-
-cd $DIRECTORY
-
-# Phoenix-raw data  and BT-settl raw data
-#wget "https://www.dropbox.com/s/raw/skg8zwi7vnxgesj/data_raw.tar.gz" -UseBasicParsing -OutFile "data_raw.tar.gz"
-#dir
-#python ..\..\scripts\untar_here.py ".\data_raw.tar.gz"
-#del "data_raw.tar.gz"
-python ..\..\scripts\download\download_test_aces.py
-
+wget "https://www.dropbox.com/s/raw/uab283lmkaptsib/Average_TAPAS_2014.dat.tar.gz" -UseBasicParsing -OutFile "$DIRECTORY2\Average_TAPAS_2014.dat.tar.gz"
+cd $DIRECTORY2
+python ..\..\scripts\untar_here.py "Average_TAPAS_2014.dat.tar.gz"
 cd $HOMEDIR
 
-# # Phoenix-raw data
-# wget "https://www.dropbox.com/s/raw/09fqard9l5s87zo/phoenix-raw.tar.gz" -UseBasicParsing -OutFile "phoenix-raw.tar.gz"
-# dir
-# python ..\..\scripts\untar_here.py ".\phoenix-raw.tar.gz"
-# del "phoenix-raw.tar.gz"
-#
-# cd $HOMEDIR
 
-
-# # BTsettl
-$DIRECTORY = ".\data\test_data\btsettl-raw\"
-
-Write-Host $DIRECTORY
-
-if ( -not (Test-Path $DIRECTORY -PathType Container))
+# ACES test data
+Write-Host $DIRECTORY3
+if ( -not (Test-Path $DIRECTORY3 -PathType Container))
 {
-Write-Host "$DIRECTORY is not present. Creating"
-mkdir $DIRECTORY
+Write-Host "$DIRECTORY3 is not present. Creating"
+mkdir $DIRECTORY3
 }
-cd $DIRECTORY
+python ..\..\scripts\download\download_test_aces.py
+
+
+# BTsettl test data
+Write-Host $DIRECTORY4
+if ( -not (Test-Path $DIRECTORY4 -PathType Container))
+{
+Write-Host "$DIRECTORY4 is not present. Creating"
+mkdir $DIRECTORY4
+}
+cd $DIRECTORY4
 wget https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011_2015/FITS/lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz -UseBasicParsing -OutFile "lte039.0-4.5-0.0a+0.0.BT-Settl.spec.fits.gz"
 cd $HOMEDIR
